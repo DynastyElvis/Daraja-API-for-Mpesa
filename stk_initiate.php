8acf3f4a09731dc4f9a2cf3d05e51c39b31ead00
Kipkemoi Elvis <kipkemoilvs@gmail.com>
2022-08-31 12:38:06 +0300

    feat:appintegration with daraja credentials

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,8 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = '2lFzD9RBnohnk1aKMAEdKdQ2TMEfSETJ'; //Fill with your app Consumer Key
+  $consumerSecret = 'Lfek4YLrLvzQbZop'; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
   # callback url
-  $CallBackURL = 'https://pesabox.herokuapp.com/callback_url.php';  
+  $CallBackURL = 'https://mpesa-by-elvis.herokuapp.com/';  
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
